c2876bba6304422f838b69c5eb741391653ea105
Snackhole <37544796+Snackhole@users.noreply.github.com>
2020-06-16 11:37:16 -0500

    Progress on MainWindow

--- Interface/MainWindow.py
+++ Interface/MainWindow.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QApplication
 
 
 class MainWindow(QMainWindow):
@@ -15,4 +16,43 @@ class MainWindow(QMainWindow):
         self.show()
 
     def CreateInterface(self):
-        pass
+        # Create Window Icon
+        # self.WindowIcon = QIcon(self.GetResourcePath("Assets/SerpentHash Icon.png"))
+
+        # Window Icon and Title
+        # self.setWindowIcon(self.WindowIcon)
+        self.setWindowTitle(self.ScriptName)
+
+        # Widgets
+
+        # Create Status Bar
+        self.StatusBar = self.statusBar()
+
+        # Center Window
+        self.Center()
+
+        # Initial Focus
+
+    def GetResourcePath(self, RelativeLocation):
+        return self.AbsoluteDirectoryPath + "/" + RelativeLocation
+
+    # Interface Methods
+    def DisplayMessageBox(self, Message, Icon=QMessageBox.Information, Buttons=QMessageBox.Ok, Parent=None):
+        MessageBox = QMessageBox(self if Parent is None else Parent)
+        MessageBox.setWindowIcon(self.WindowIcon)
+        MessageBox.setWindowTitle(self.ScriptName)
+        MessageBox.setIcon(Icon)
+        MessageBox.setText(Message)
+        MessageBox.setStandardButtons(Buttons)
+        return MessageBox.exec_()
+
+    def FlashStatusBar(self, Status, Duration=2000):
+        self.StatusBar.showMessage(Status)
+        QTimer.singleShot(Duration, self.StatusBar.clearMessage)
+
+    # Window Management Methods
+    def Center(self):
+        FrameGeometryRectangle = self.frameGeometry()
+        DesktopCenterPoint = QApplication.primaryScreen().availableGeometry().center()
+        FrameGeometryRectangle.moveCenter(DesktopCenterPoint)
+        self.move(FrameGeometryRectangle.topLeft())
