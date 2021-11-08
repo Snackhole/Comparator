a22dd3cb6e3a9a28a5f06a33bae5db485e50fc38
Snackhole <37544796+Snackhole@users.noreply.github.com>
2021-11-07 21:19:29 -0600

    Bump to version 5

--- Build.py
+++ Build.py
@@ -7,7 +7,7 @@ import zipapp
 
 # Build Variables
 BuildVariables = {}
-BuildVariables["Version"] = "4"
+BuildVariables["Version"] = "5"
 BuildVariables["AppName"] = "SerpentHash"
 BuildVariables["VersionedAppName"] = BuildVariables["AppName"] + " " + BuildVariables["Version"]
 
