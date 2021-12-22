07f4f29e18d9cfc35ea6b0a0eb2f35580f3d1b33
Snackhole <37544796+Snackhole@users.noreply.github.com>
2021-12-22 16:08:15 -0600

    Bump to 6

--- Build.py
+++ Build.py
@@ -7,7 +7,7 @@ import zipapp
 
 # Build Variables
 BuildVariables = {}
-BuildVariables["Version"] = "5"
+BuildVariables["Version"] = "6"
 BuildVariables["AppName"] = "Comparator"
 BuildVariables["VersionedAppName"] = BuildVariables["AppName"] + " " + BuildVariables["Version"]
 
